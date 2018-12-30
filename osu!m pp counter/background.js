40ec864c85d62bcc084b4347828e65174d933081
TheEZIC <35061249+TheEZIC@users.noreply.github.com>
2018-12-31 00:58:21 +0300

    1.2

--- osu!m pp counter/background.js
+++ osu!m pp counter/background.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
   
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(tabs[0].id, {popupOpen: true}, function(response) {
+			console.log('Отправленно состояние открытия');
 		});
   });
 });
